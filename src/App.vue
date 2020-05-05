c79d1dbedfe5c03247344d70a3f3b533c883e2af
algol007 <adhyformerz@gmail.com>
2020-05-06 01:05:22 +0800

    add: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <navbar />
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Rubik:wght@500&display=swap');
@@ -15,7 +25,13 @@
     margin: 0;
     padding: 0;
   }
-
+  body{
+  }
+  #app{
+    min-height: 100vh;
+    background-color: #2c3e50;
+    color: #ffffff;
+  }
   .evn-primary{
     background-color: #2c3e50;
   }
