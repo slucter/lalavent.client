5dfc119a88398a1c31812909f2540666132e0d4b
Firman <fblazt@outlook.com>
2020-05-07 04:59:34 +0700

    Add event card to organizer event list

--- src/views/AddEvent.vue
+++ src/views/AddEvent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-5">
+  <div class="container my-5">
     <div class="row">
       <div class="col">
         <div class="form-group">
--- src/views/OrganizerEventList.vue
+++ src/views/OrganizerEventList.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="container mt-5">
+  <div class="container container-fluid my-5">
     <OrganizerCard/>
-    <div class="px-3 mt-5 d-flex flex-row justify-content-between">
-      <h3 class="evn-title text-white">Semua Event</h3>
+    <div class="px-3 mt-5 d-flex flex-row justify-content-between align-items-center">
+      <h3 class="mb-0 evn-title text-white">Semua Event</h3>
       <Button>
         <router-link class="link-anchor" to="/:organizerId/add-event">Buat Event</router-link>
       </Button>
+    </div>
+    <SearchSort class="mt-5 px-5"/>
+    <div class="mt-3 d-flex flex-wrap justify-content-center">
+      <CardEvent class="evn-shadow" v-for="item in 8" :key="item"/>
     </div>
   </div>
 </template>
@@ -13,12 +17,16 @@
 <script>
 import OrganizerCard from '@/components/OrganizerCard'
 import Button from '@/components/Button'
+import SearchSort from '@/components/SearchSort'
+import CardEvent from '@/components/EventList/CardEvent'
 
 export default {
   name: 'OrganizerEventlist',
   components: {
     OrganizerCard,
-    Button
+    Button,
+    SearchSort,
+    CardEvent
   }
 }
 </script>
